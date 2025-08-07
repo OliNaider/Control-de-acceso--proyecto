9965fbfc64b3463827b7abc802b50ac2f68ab5b0
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-08-07 20:33:07 -0300

    SE CAMBIA LA CLAVEEEEE

--- prueba5/prueba5.ino
+++ prueba5/prueba5.ino
@@ -13,14 +13,15 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*', '0', '#', 'D'}
 };
 
-byte pin_rows[ROW_NUM] = {12, 11, 10, 9}; // GPIO18, GPIO5, GPIO17, GPIO16 connect to the row pins
-byte pin_column[COLUMN_NUM] = {8, 7, 6, 5};  // GPIO4, GPIO0, GPIO2 connect to the column pins
+byte pin_rows[ROW_NUM] = {19, 18, 5, 17}; // GPIO18, GPIO5, GPIO17, GPIO16 connect to the row pins
+byte pin_column[COLUMN_NUM] = {16, 4, 0, 2};  // GPIO4, GPIO0, GPIO2 connect to the column pins
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
  
 String input_password;
 const String cambioClave = "123";
-String password;
+String password = "7890";
+int estado = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -33,52 +34,86 @@ void setup() {
 }
 
 void loop() {
-  password = "7890";
   char key = keypad.getKey();
 
   if (key) {
     
-    switch (key) {
+    Serial.println(key);  
+    lcd.print("*");
 
-      Serial.println(key);  
-      lcd.print("*");
+    switch (key) {
 
       case '*':
         input_password = ""; // limpiar input password
         lcd.clear();
+        Serial.println(password);
         break;
 
       case '#':
-        if (password == input_password) {
-          Serial.println("The password is correct, ACCESS GRANTED!");
-          lcd.clear();
-          lcd.print("The password is");
-          lcd.setCursor(2, 1);
-          lcd.print("correct");
-          delay(500);
-          lcd.clear();
+        if(estado == 0) {
+          if (password == input_password) {
+            Serial.println("The password is correct, ACCESS GRANTED!");
+            lcd.clear();
+            lcd.print("The password is");
+            lcd.setCursor(2, 1);
+            lcd.print("correct");
+            delay(500);
+            lcd.clear();
 
-          if (input_password == cambioClave) {
-            Serial.println("ingrese nueva clave");
-            password = key;  // Revisa esta parte, tal vez quieres otra cosa
+            estado = 1;
+
+            Serial.println(password);
+            Serial.println(estado);
+
+          } else {
+            Serial.println("The password is incorrect, ACCESS DENIED!");
+            lcd.clear();
+            lcd.print("The password is incorrect");
+            lcd.setCursor(2, 1);
+            lcd.print("incorrect");
+            delay(500);
+            lcd.clear();
+
+            estado = 0;
+            Serial.println(estado);
             Serial.println(password);
           }
-        } else {
-          Serial.println("The password is incorrect, ACCESS DENIED!");
-          lcd.clear();
-          lcd.print("The password is incorrect");
-          lcd.setCursor(2, 1);
-          lcd.print("incorrect");
-          delay(500);
-          lcd.clear();
+
+          input_password = "";
+
+        } else if(estado == 1){
+          if(input_password == "123"){
+            Serial.println("ingrese la nueva clave");
+            estado = 2;
+            Serial.println(password);
+            Serial.println(estado);
+          } else {
+            estado = 0;
+            Serial.println("no se ha cambiado la clave");
+            Serial.println(password);
+            Serial.println(estado);
+          }
+
+          input_password = "";
+
+        }else if(estado == 2) {
+          password = input_password;
+          Serial.println(password);
+          Serial.println("clave cambiada");
+          Serial.println(password);
+          estado = 0;
+          Serial.println(estado);
+
+          input_password = "";
         }
 
-        input_password = ""; // limpiar input password
         break;
 
       default:
         input_password += key; // agregar carácter a input password
         break;
-    }
+    } 
   }
 }
+
+
