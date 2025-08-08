d79b237855ce3dcbe0aeec34322f6dcabfb59deb
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-08-08 09:01:14 -0300

    blablaab

--- prueba5/prueba5.ino
+++ prueba5/prueba5.ino
@@ -13,8 +13,8 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*', '0', '#', 'D'}
 };
 
-byte pin_rows[ROW_NUM] = {19, 18, 5, 17}; // GPIO18, GPIO5, GPIO17, GPIO16 connect to the row pins
-byte pin_column[COLUMN_NUM] = {16, 4, 0, 2};  // GPIO4, GPIO0, GPIO2 connect to the column pins
+byte pin_rows[ROW_NUM] = {12, 11, 10, 9}; // GPIO18, GPIO5, GPIO17, GPIO16 connect to the row pins
+byte pin_column[COLUMN_NUM] = {8, 7, 6, 5};  // GPIO4, GPIO0, GPIO2 connect to the column pins
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
  
