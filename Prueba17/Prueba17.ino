d6d96b6afc9aa65faf58593f93cb08568f375bd2
NAIDERMAN OLIVIA <49521150@ort>
2025-09-01 09:58:54 -0300

    manda maiilllll con fotito

---
+++ Prueba17/Prueba17.ino
@@ -0,0 +1,283 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "board_config.h"
+#include <esp_now.h>
+//MAIL:
+#include "SPI.h"
+#include "driver/rtc_io.h"
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include <ESP_Mail_Client.h>
+#include <LittleFS.h>
+
+// ==== WIFI para streaming ====
+const char *ssid = "moto g(7) plus 5062";
+const char *password = "0afa3b8c20d8";
+void startCameraServer();
+
+// ==== ESP-NOW ====
+typedef struct struct_message {
+  char msg[32];
+} struct_message;
+
+struct_message incomingData;
+String datosRecibidos = "";
+
+// Callback ESP-NOW
+void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingDataBytes, int len) {
+  memcpy(&incomingData, incomingDataBytes, sizeof(incomingData));
+  Serial.print("ESP-NOW recibido: ");
+  Serial.println(incomingData.msg);
+  datosRecibidos = incomingData.msg;
+}
+
+//mail
+#define emailSenderAccount    "IngresoSeguridadControl@gmail.com"
+#define emailSenderPassword   "kvye pqwj sbkc xkpo"
+#define smtpServer            "smtp.gmail.com"
+#define smtpServerPort        465
+#define emailSubject          "ESP32-CAM Photo Captured"
+#define emailRecipient        "olinaider@gmail.com"
+SMTPSession smtp;
+void smtpCallback(SMTP_Status status);
+#define FILE_PHOTO "photo.jpg"
+#define FILE_PHOTO_PATH "/photo.jpg"
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  // ==== MODO WIFI MIXTO (AP + STA) ====
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  Serial.print("Conectando a WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi conectado");
+
+  // ==== INICIO ESP-NOW ====
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error iniciando ESP-NOW");
+    return;
+  }
+  esp_now_register_recv_cb(OnDataRecv);
+
+  //INICIO littleFS
+  ESP_MAIL_DEFAULT_FLASH_FS.begin();
+
+
+  // ==== INICIO CÁMARA ====
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  if (psramFound()) {
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+  }
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("Error iniciando la cámara");
+    return;
+  }
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' para conectar");
+}
+
+void loop() {
+  if (datosRecibidos == "FOTO") {
+    Serial.println("fotito");
+    datosRecibidos = "";  // limpiar para que no se repita
+    capturePhotoSaveLittleFS();
+    sendPhoto();
+  }
+}
+
+// Capture Photo and Save it to LittleFS
+void capturePhotoSaveLittleFS( void ) {
+  // Dispose first pictures because of bad quality
+  camera_fb_t* fb = NULL;
+  // Skip first 3 frames (increase/decrease number as needed).
+  for (int i = 0; i < 3; i++) {
+    fb = esp_camera_fb_get();
+    esp_camera_fb_return(fb);
+    fb = NULL;
+  }
+    
+  // Take a new photo
+  fb = NULL;  
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }  
+
+  // Photo file name
+  Serial.printf("Picture file name: %s\n", FILE_PHOTO_PATH);
+  File file = LittleFS.open(FILE_PHOTO_PATH, FILE_WRITE);
+
+  // Insert the data in the photo file
+  if (!file) {
+    Serial.println("Failed to open file in writing mode");
+  }
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.print("The picture has been saved in ");
+    Serial.print(FILE_PHOTO_PATH);
+    Serial.print(" - Size: ");
+    Serial.print(fb->len);
+    Serial.println(" bytes");
+  }
+  // Close the file
+  file.close();
+  esp_camera_fb_return(fb);
+}
+
+void sendPhoto( void ) {
+  
+  /** Enable the debug via Serial port
+   * none debug or 0
+   * basic debug or 1
+  */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  Session_Config config;
+  
+  /*Set the NTP config time
+  For times east of the Prime Meridian use 0-12
+  For times west of the Prime Meridian add 12 to the offset.
+  Ex. American/Denver GMT would be -6. 6 + 12 = 18
+  See https://en.wikipedia.org/wiki/Time_zone for a list of the GMT/UTC timezone offsets
+  */
+  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+  config.time.gmt_offset = -3 * 3600;  // en segundos (-10800)
+  config.time.day_light_offset = 0;    // sin horario de verano
+
+  /* Set the session config */
+  config.server.host_name = smtpServer;
+  config.server.port = smtpServerPort;
+  config.login.email = emailSenderAccount;
+  config.login.password = emailSenderPassword;
+  config.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Enable the chunked data transfer with pipelining for large message if server supported */
+  message.enable.chunking = true;
+
+  /* Set the message headers */
+  message.sender.name = "ESP32-CAM";
+  message.sender.email = emailSenderAccount;
+
+  message.subject = emailSubject;
+  message.addRecipient("Sara", emailRecipient);
+
+  String htmlMsg = "<h2>Photo captured with ESP32-CAM and attached in this email.</h2>";
+  message.html.content = htmlMsg.c_str();
+  message.html.charSet = "utf-8";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_qp;
+
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* The attachment data item */
+  SMTP_Attachment att;
+
+  /** Set the attachment info e.g. 
+   * file name, MIME type, file path, file storage type,
+   * transfer encoding and content encoding
+  */
+  att.descr.filename = FILE_PHOTO;
+  att.descr.mime = "image/png"; 
+  att.file.path = FILE_PHOTO_PATH;
+  att.file.storage_type = esp_mail_file_storage_type_flash;
+  att.descr.transfer_encoding = Content_Transfer_Encoding::enc_base64;
+
+  /* Add attachment to the message */
+  message.addAttachment(att);
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&config))
+    return;
+
+  /* Start sending the Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message, true))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+}
+
+// Callback function to get the Email sending status
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success())
+  {
+    Serial.println("----------------");
+    Serial.printf("Message sent success: %d\n", status.completedCount());
+    Serial.printf("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+    }
+    Serial.println("----------------\n");
+
+   // You need to clear sending result as the memory usage will grow up.
+   smtp.sendingResult.clear();
+  }
+}
+
---
+++ Prueba17/board_config.h
@@ -0,0 +1,5 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+#include "camera_pins.h"
+#endif  // BOARD_CONFIG_H
