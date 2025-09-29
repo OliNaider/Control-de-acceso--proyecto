d0a6e5cf3bab331a913d1048618d67259a170ffc
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-09-29 10:15:18 -0300

    reorganizando carpetas

---
+++ Resto del codigo/prueba_finalCarrera/prueba_finalCarrera.ino
@@ -0,0 +1,27 @@
+// Conexión:
+// COM -> GND
+// NO  -> pin 2
+// Usa el LED integrado (Arduino UNO: 13, ESP32: 2 normalmente)
+
+const int PIN_SWITCH = 2;    
+const int PIN_LED = 13;   // En ESP32 puede ser el 2
+
+void setup() {
+  pinMode(PIN_SWITCH, INPUT_PULLUP); // pullup interno
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (digitalRead(PIN_SWITCH) == LOW) { 
+    // Pulsado
+    digitalWrite(PIN_LED, HIGH);
+    Serial.println("Pulsado");
+  } else {
+    // No pulsado
+    digitalWrite(PIN_LED, LOW);
+    Serial.println("No pulsado");
+  }
+  delay(100);
+}
+
